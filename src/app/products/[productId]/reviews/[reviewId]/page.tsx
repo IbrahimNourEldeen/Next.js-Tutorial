3bf719f7d7057e9ec7a42c306037f74b0a92216a
Ibrahim Noureldeen <ibrahimnoureldeen11@gmail.com>
2026-01-17 18:27:10 +0200

    Not Found Page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 export default async function ReviewDetails({
     params,
 }:{
     params: Promise<{reviewId:string, productId:string}>
 }) {
     const {reviewId,productId} = await params;
+    if(reviewId === '0'){
+        notFound();
+    }
     return <h1>revew {reviewId} for  product {productId}</h1>;
 }
