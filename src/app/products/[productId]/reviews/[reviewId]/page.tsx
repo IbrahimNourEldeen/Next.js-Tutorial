59f4a6c9e1bfa3513518461ee69bffeb21793308
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com.com>
2026-01-23 15:05:42 +0200

    error.tsx

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,12 +1,22 @@
 import { notFound } from "next/navigation";
+
+function gerRandomInt(count:number){
+    return Math.floor(Math.random()*count);
+}
+
 export default async function ReviewDetails({
     params,
 }:{
     params: Promise<{reviewId:string, productId:string}>
 }) {
     const {reviewId,productId} = await params;
-    if(reviewId === '0'){
+    const random = gerRandomInt(2);
+
+    if (random == 1) throw new Error('error loading review');
+
+    if(reviewId >= '1000'){
         notFound();
     }
+
     return <h1>revew {reviewId} for  product {productId}</h1>;
 }
