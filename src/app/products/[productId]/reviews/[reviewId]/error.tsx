4395cbcc30f1b344b6f45c42cb4f86bc3699c83a
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com.com>
2026-01-24 08:15:00 +0200

    Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,4 +1,17 @@
-'use client'
-export default function ({error}:{error:Error}){
-    return <div>{error.message}</div>
-};
+"use client";
+export default function ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      {error.message}
+      <button className="btn ms-3 border rounded bg-cyan-500 text-white p-1" onClick={reset}>
+        try again
+      </button>
+    </div>
+  );
+}
