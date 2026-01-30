eddc8cc761af2b18019b769663eb8f8fe9719bab
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com>
2026-01-30 18:42:24 +0200

    context provider

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -1,12 +1,19 @@
 "use client";
-import { useTheme } from "./themeProvider"; 
+import { useTheme } from "./themeProvider";
 
 export function ThemeSwitcher() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme}>
-        {theme === "light" ? "Dark 🌙" : "Light ☀️"}
+    <button
+      onClick={toggleTheme}
+      className={
+        theme === "light"
+          ? "bg-gray-800 text-white absolute p-2 rounded-2xl"
+          : "bg-white text-gray-800 absolute p-2 rounded-2xl"
+      }
+    >
+      {theme === "light" ? "Dark 🌙" : "Light ☀️"}
     </button>
   );
 }
